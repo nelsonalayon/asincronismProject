a5dce9a250b940d8bb1cf451e586b2ed35fae99c
nelsonalayon <nelson.alayon@gmail.com>
2024-06-04 18:19:10 -0500

    custom site

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,7 @@
 const content = null || document.getElementById("content");
 
+// se necesita un codigo en el sitio web de youtube del que se quiere obtener los datos , este se obtiene en la url del canal de youtube
+
 const url =
   "https://youtube-v3-alternative.p.rapidapi.com/channel?id=UCeptR6w7QTmADvFf7VI6MNQ";
 const options = {
@@ -26,11 +28,11 @@ async function fetchData(urlApi) {
         (video) => `
     <div class="group relative">
     <div
-      class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+      class="w-full bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:aspect-none">
       <img src="${video.thumbnail[0].url}" alt="" class="w-full">
     </div>
     <div class="mt-4 flex justify-between">
-      <h3 class="text-sm text-gray-700">
+      <h3 class="text-sm text-gray-100">
         <span aria-hidden="true" class="absolute inset-0"></span>
         ${video.title}
       </h3>
